2fe0af4777a3b4a71eb0777f63151d7f52b9a10e
De-Silva-Madhushankha <madhushankhades1@gmail.com>
2025-07-09 18:08:56 +0530

    Add custom tool function to retrieve current time

--- google/tool_agent/agent.py
+++ google/tool_agent/agent.py
@@ -1,10 +1,22 @@
 from google.adk.agents import Agent
 from google.adk.tools import google_search
 
+### Best practice: Define custom tools for specific functionalities -> Specify return types and descriptions, return meaningful and descriptive data instead of just results
+
+def get_current_time() -> dict:
+    """Get the current time in the format YYYY-MM-DD HH:MM:SS."""
+    from datetime import datetime
+    now = datetime.now()
+    return {
+        "current_time": now.strftime("%Y-%m-%d %H:%M:%S")
+    }
+
 root_agent = Agent(
     name="tool_agent",
     model="gemini-2.0-flash",
     description="Tool Agent that can perform Google searches.",
     instruction="""You are a tool agent that can perform Google searches. If the user asks a question, use the google_search tool to find relevant information and provide a concise answer.""",
-    tools=[google_search],
+    #tools=[get_current_time],  # Custom tool to get current time
+    tools=[google_search], # Can only use one built-in tool at a time
+    #tools=[google_search, get_current_time], # Doesn't work with multiple tools yet
 )
